e3e03653539a4b280f2a083fd2eafec3e4e39c9e
imxande <alerodriguezlarosa@gmail.com>
2020-07-18 02:08:00 -0400

    rendering poke names

--- port/src/App.js
+++ port/src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import PokemonList from "./components/PokemonList";
+import PokemonCard from "./components/PokemonCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <PokemonList />
       </header>
     </div>
--- port/src/components/PokemonList.js
+++ port/src/components/PokemonList.js
@@ -1,18 +1,25 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import PokemonCard from "./PokemonCard";
 
 const PokemonList = () => {
   // state
-  const [pokemon, setPokemon] = useState();
+  const [pokemon, setPokemon] = useState([]);
 
   useEffect(() => {
     axios
       .get("https://pokeapi.co/api/v2/pokemon?offset=20&limit=20")
-      .then((response) => console.log(response.data.results))
+      .then((response) => setPokemon(response.data.results))
       .catch((error) => console.log(error));
   }, []);
 
-  return <div>I am connected</div>;
+  return (
+    <div>
+      {pokemon.map((poke) => {
+        return <PokemonCard name={poke.name} />;
+      })}
+    </div>
+  );
 };
 
 export default PokemonList;
