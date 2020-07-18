7417b6071963d025f036990f70f47a7c6963ee44
imxande <alerodriguezlarosa@gmail.com>
2020-07-18 19:49:57 -0400

    added navegation

--- port/src/App.js
+++ port/src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./App.css";
 import PokemonList from "./components/PokemonList";
 import Home from "./components/Home";
-import NavBar from "./components/NavBar";
+import Navegation from "./components/Navegation";
 import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <NavBar />
+      <Navegation />
       <Route exact path="/" component={Home} />
       <Route path="/list" component={PokemonList} />
     </div>
