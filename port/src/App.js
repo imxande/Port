c3774cf636a02588c710d01e246b87fa9d7906f6
imxande <alerodriguezlarosa@gmail.com>
2020-07-17 22:44:38 -0400

    added boilerplate for pokelist

--- port/src/App.js
+++ port/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import PokemonList from "./components/PokemonList";
 
 function App() {
   return (
---
+++ port/src/components/PokemonList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PokemonList = () => {
+  return <div>I am connected</div>;
+};
+
+export default PokemonList;
