bf6b81bf81067f6d7cf929bcb64f28ecb9d1003e
imxande <alerodriguezlarosa@gmail.com>
2020-07-17 22:53:59 -0400

    fetching poke data

--- port/src/App.js
+++ port/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React is awesome!</h1>
+        <PokemonList />
       </header>
     </div>
   );
--- port/src/components/PokemonList.js
+++ port/src/components/PokemonList.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const PokemonList = () => {
+  // state
+  const [pokemon, setPokemon] = useState();
+
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/evolution-chain/?limit=20&offset=20")
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  }, []);
+
   return <div>I am connected</div>;
 };
 
