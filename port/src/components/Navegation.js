706e50226f9be730ca8ec329775b200c6c329e97
imxande <alerodriguezlarosa@gmail.com>
2020-07-19 03:42:39 -0400

    styled links using reacstrap navbar

--- port/src/components/Navegation.js
+++ port/src/components/Navegation.js
@@ -2,15 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/Navbar";
 
 const Navegation = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="#home">Pokemon</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav"></Navbar.Collapse>
-    </Navbar>
+    <div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Pokemon</Navbar.Brand>
+        <Nav className="navbar-nav ml-auto">
+          <Link className="link" to="/">
+            Home
+          </Link>
+          <Link className="link" to="/list">
+            Pokemon List
+          </Link>
+        </Nav>
+      </Navbar>
+    </div>
   );
 };
 
